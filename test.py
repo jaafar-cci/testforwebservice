818245596dced4f2f5d0e4e88ccb9068eea7a635
jaafar-cci <123546197+jaafar-cci@users.noreply.github.com>
2023-05-30 10:19:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ question = st.text_input("how can I help you today?", "")
 
 # Establish a connection to the SQL Server database
 conn = pyodbc.connect(
-    'Driver={SQL Server};'
+    'Driver={SQL Server Native Client 11.0};'
     "uid=userj;pwd=useruser;"
     'Server=Mic;'
     'Database=testopenai;'

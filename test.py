8066103aa5e04884c4329935fb0da23f8fb04008
jaafar-cci <123546197+jaafar-cci@users.noreply.github.com>
2023-05-30 11:10:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -27,9 +27,9 @@ question = st.text_input("how can I help you today?", "")
 
 # Establish a connection to the SQL Server database
 conn = pyodbc.connect(
-    'Driver={SQL Server Native Client RDA 11.0};'
+    'Driver={ODBC Driver 17 for SQL Server};'
     "uid=userj;pwd=useruser;"
-    'Server=Mic;'
+    'Server=Mic\MSSQLSERVER;'
     'Database=testopenai;'
     'Trusted_Connection=yes;')
        
